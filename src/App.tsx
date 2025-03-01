4143949236c0c47fb534c584b2e8cf30954aeaa7
cosmiclatte <129236813+coslatte@users.noreply.github.com>
2025-03-01 02:03:19 -0500

    Modified main page React components

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
-import { useState } from "react";
+import TaskList from "./TaskList"
 import "./styles/index.css";
 
-function Task() {
-  const [taskInfo, setTaskInfo] = useState("");
-
+const App: React.FC = () => {
   return (
     <>
-      <div className="bg-purple-200 flex space-x-4 p-4">
-        {/* Botón Completar Tarea */}
-        <div>
-          <button className="btn-rounded-purple">Agregar</button>
-        </div>
-        {/* Texto con información de Tarea */}
-        <div>
-          <input
-            type="text"
-            className="border p-2 rounded-2xl border-purple-100"
-            placeholder="Tarea aqui..."
-          />
-        </div>
-        {/* Editar Tarea */}
-        <div>
-          <button className="btn-rounded-purple">Editar</button>
-        </div>
-        {/* Eliminar Tarea */}
-        <div>
-          <button className="btn-rounded-purple">Elimitar</button>
-        </div>
-      </div>
+      <TaskList/>
     </>
   );
-}
-
-function App() {
-  return (
-    <>
-      <div className="bg-gray-800">
-        <Task />
-      </div>
-    </>
-  );
-}
+};
 
 export default App;
